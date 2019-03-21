4b45917edf37ef1df5fd35c4451796a17c3bf72d
Viktor Brusylovets <v.brusylovets@gmail.com>
2019-03-21 21:58:12 +0100

    logger got back

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+import { createLogger, format, transports } from 'winston';
+import dotenv from 'dotenv';
 import { ApolloServer } from 'apollo-server';
 import { resolvers } from './src/resolvers';
 import { typeDefs } from './src/type-defs';
-import dotenv from 'dotenv';
+import config from './src/config';
 
 dotenv.config();
+
+const logger = createLogger({
+    format: format.combine(
+      format.splat(),
+      format.colorize(),
+      format.align(),
+      format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+      }),
+      format.printf(info => `${info.timestamp} ${info.level}: ${info instanceof Error ? JSON.stringify(info.stack) : info.message}`),
+    ),
+    transports: [
+        new transports.Console({
+            level: config.get('logLevel'),
+        }),
+    ],
+});
 
 const server = new ApolloServer({
     typeDefs,
@@ -11,5 +30,5 @@ const server = new ApolloServer({
 });
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
+    logger.info(`🚀 Server ready at ${url}`);
 });
