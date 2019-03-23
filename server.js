bfb4322b6bc6adf2140ee79d88f86d038317ad2d
Viktor Brusylovets <v.brusylovets@gmail.com>
2019-03-23 10:41:43 +0100

    refactored resolvers

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 import { createLogger, format, transports } from 'winston';
 import dotenv from 'dotenv';
 import { ApolloServer } from 'apollo-server';
-import { resolvers } from './src/resolvers';
+import { createResolvers } from './src/resolvers';
 import { typeDefs } from './src/type-defs';
 import config from './src/config';
+// import { PubSub } from 'graphql-subscriptions';
+import { ConsoleLogger } from "@cdm-logger/server";
+import { AmqpPubSub } from 'graphql-rabbitmq-subscriptions';
+
+// const pubsub = new PubSub();
+
+const RABBITMQ_HOST = process.env.RADAR_RABBITMQ_HOST;
+const RABBITMQ_PORT = process.env.RADAR_RABBITMQ_PORT;
+
+const settings = {
+  level: "info", // Optional: default 'info' ('trace'|'info'|'debug'|'warn'|'error'|'fatal')
+  mode: "short" // Optional: default 'short' ('short'|'long'|'dev'|'raw')
+};
+
+const consoleLogger = ConsoleLogger.create("radar", settings);
+
+const pubsub = new AmqpPubSub({
+  logger: consoleLogger,
+  config: {host: RABBITMQ_HOST, port: RABBITMQ_PORT}
+});
 
 dotenv.config();
 
@@ -24,9 +44,11 @@ const logger = createLogger({
     ],
 });
 
+const resolvers = createResolvers(pubsub);
+
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
 
 server.listen().then(({ url }) => {
